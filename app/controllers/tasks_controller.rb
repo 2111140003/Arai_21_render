7bc91887288ac0a8e98584c205d684ac3b2f4142
Arai Taichi <ubuntu@ip-172-31-18-145.ec2.internal>
2023-12-06 09:06:07 +0000

    タスク編集画面追加

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,29 @@
+class TasksController < ApplicationController
+  def index
+    @tasks=Task.all
+    @task=Task.new
+  end
+  def edit
+    @task = Task.find(params[:id])
+  end
+  def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to root_path
+  end
+  def create
+    task = Task.new(task_name: params[:task][:task_name],task_det: params[:task][:task_det])
+    task.save
+    redirect_to root_path
+  end
+  def destroy
+    task = Task.find(params[:id])
+    if task.user_id == current_user.id
+        task.destroy
+    end
+  end
+  private
+  def task_params
+    params.require(:task).permit(:task_name, :task_det)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'task/new'
-  get 'task/create'
-  get 'task/edit'
+  root "tasks#index"
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
   get 'team/new'
   get 'team/create'
-  root "session#login"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :tasks , only: [:create, :new, :edit, :update, :destroy]
+  get 'tasks/create'
+  post 'tasks/create'
+  
+  
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
