290c4f1a7f458161433fdc2a2fad2e2d53eddce2
2111140079f <2111140079f@ed.fuk.kindai.ac.jp>
2023-12-06 09:07:52 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   get 'task/edit'
   get 'team/new'
   get 'team/create'
-  root "session#login"
+  resources :sessions, only: [:new, :create]
+  resources :user
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#login'
+  delete '/logout',  to: 'sessions#destroy'
+  root "sessions#login"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_075202) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_06_062228) do
+  create_table "accounts", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "tasks", force: :cascade do |t|
     t.integer "task_id"
     t.string "task_name"
@@ -28,12 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_22_075202) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.integer "user_id"
-    t.string "user_name"
-    t.string "password"
-    t.integer "team_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "uid"
   end
 
