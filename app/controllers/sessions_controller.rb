03c14998caafc76b321ed143d85a220611e03f80
2111140079f <2111140079f@ed.fuk.kindai.ac.jp>
2023-12-13 08:22:44 +0000

    ログインページからtodoリストへ遷移

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     consumer = Consumer.find_by_email(params[:email])
     if consumer && consumer.authenticate(params[:password])
       session[:consumer_id] = consumer.id
-      redirect_to consumer_path, notice: "You have successfully logged in."
+      redirect_to tasks_path, notice: "You have successfully logged in."
     else
       flash.now[:alert] = "Email or Password is invalid."
       render :new
--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,13 @@
 Rails.application.routes.draw do
-  root  'homes#top'
+  root 'homes#top'
   resource :consumer, only: [:new, :create, :show]
   get 'login', to: "sessions#new"
   post 'login', to: "sessions#create"
   delete 'logout', to: "sessions#destroy"
-  #root "tasks#index"
-#   patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
-#   get 'team/new'
-#   get 'team/create'
-#   resources :sessions, only: [:new, :create]
-#   resources :user
-#   get    '/login',   to: 'sessions#new'
-#   post   '/login',   to: 'sessions#login'
-#   delete '/logout',  to: 'sessions#destroy'
-#   root "sessions#login"
-#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-#   resources :tasks , only: [:create, :new, :edit, :update, :destroy]
-#   get 'tasks/create'
-#   post 'tasks/create'
-  
-  
-  
-#   # Defines the root path route ("/")
-#   # root "articles#index"
-  resources :tasks , only: [:create, :new, :edit, :update, :destroy]
-  get 'tasks/create'
-  post 'tasks/create'
-  delete 'tasks/:id' => 'tasks#destroy'
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :tasks, only: [:index, :create, :new, :edit, :update, :destroy]
+
+  # ... 他のコメントアウトされたルートや不要な行があれば確認
+
 end
+
