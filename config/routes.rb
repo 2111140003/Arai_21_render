37268a6091480839e8cade518739ebf9a688f9e9
2111140079f <2111140079f@ed.fuk.kindai.ac.jp>
2023-12-13 07:01:15 +0000

    user

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,27 @@
 Rails.application.routes.draw do
-  root "tasks#index"
-  patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
-  get 'team/new'
-  get 'team/create'
-  resources :sessions, only: [:new, :create]
-  resources :user
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#login'
-  delete '/logout',  to: 'sessions#destroy'
-  root "sessions#login"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root  'homes#top'
+  resource :consumer, only: [:new, :create, :show]
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+  #root "tasks#index"
+#   patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
+#   get 'team/new'
+#   get 'team/create'
+#   resources :sessions, only: [:new, :create]
+#   resources :user
+#   get    '/login',   to: 'sessions#new'
+#   post   '/login',   to: 'sessions#login'
+#   delete '/logout',  to: 'sessions#destroy'
+#   root "sessions#login"
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  resources :tasks , only: [:create, :new, :edit, :update, :destroy]
-  get 'tasks/create'
-  post 'tasks/create'
+#   resources :tasks , only: [:create, :new, :edit, :update, :destroy]
+#   get 'tasks/create'
+#   post 'tasks/create'
   
   
   
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_06_062228) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_13_062343) do
   create_table "accounts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "consumers", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_consumers_on_email", unique: true
   end
 
   create_table "tasks", force: :cascade do |t|
